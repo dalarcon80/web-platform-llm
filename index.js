3dddd50cb5b07d9d0ae8fed97f3f483374df34bf
dalarcon80 <63214788+dalarcon80@users.noreply.github.com>
2023-10-02 20:27:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+// Enter your token server url to connect to your project
+// NOTE: your token server must allow requests from 'https://web-platform-9chn6x.stackblitz.io'
+//const tokenServer = 'http://localhost:5001/auth/authorize';
+
+// Note that your API key must allow requests from
+// this StackBlitz demo: 'https://web-platform-9chn6x.stackblitz.io'
+// Note that the URL will change if you fork this project
 const apiKey =
-  'eyJzb3VsSWQiOiJkZG5hLWRhbmllbC1hbGFyY29uLS1kaWdpdGFsLWNoaWVmLWV4cGVyaWVuY2UiLCJhdXRoU2VydmVyIjoiaHR0cHM6Ly9kaC5hei5zb3VsbWFjaGluZXMuY2xvdWQvYXBpL2p3dCIsImF1dGhUb2tlbiI6ImFwaWtleV92MV9lNmY1ZTA3OS00ODI1LTQ2YjUtYjE5ZDkwYmQ1MTM3In0==';
+  'eyJzb3VsSWQiOiJkZG5hLWRhbmllbC1hbGFyY29uLS1kaWdpdGFsLWNoaWVmLWV4cGVyaWVuY2UiLCJhdXRoU2VydmVyIjoiaHR0cHM6Ly9kaC5hei5zb3VsbWFjaGluZXMuY2xvdWQvYXBpL2p3dCIsImF1dGhUb2tlbiI6ImFwaWtleV92MV9lNmYxZTA3OS00ODI1LTQ2YjUtYjE5ZC05YjEwYzRjNDUzNjEifQ==';
 
 let scene;
 
+/**
+ * Start a new connection.
+ * Request a JWT from the token server and use it
+ * to connect to the Soul Machines session server.
+ */
 async function connect() {
+  // get the video element
   const videoEl = document.getElementById('sm-video');
+
+  // create a new scene object
   scene = new Scene({
     apiKey: apiKey,
     videoElement: videoEl,
@@ -12,20 +27,31 @@ async function connect() {
     requiredMediaDevices: { microphone: true, camera: true },
   });
 
+  // connect the Scene to the session server
   await scene
     .connect()
     .then((sessionId) => onConnectionSuccess(sessionId))
     .catch((error) => onConnectionError(error));
 }
 
+/**
+ * Handle successful connection
+ * On success, we must start the video.
+ */
 function onConnectionSuccess(sessionId) {
   console.info('success! session id:', sessionId);
+
+  // start the video playing
   scene
     .startVideo()
     .then((videoState) => console.info('started video with state:', videoState))
     .catch((error) => console.warn('could not start video:', error));
 }
 
+/**
+ * Handle failed connection
+ * On error, we must display some feedback to the user
+ */
 function onConnectionError(error) {
   switch (error.name) {
     case 'noUserMedia':
@@ -40,27 +66,12 @@ function onConnectionError(error) {
   }
 }
 
+/**
+ * Event listeners for button in the HTML
+ */
+
 const connectButton = document.getElementById('connect-button');
 connectButton.addEventListener('click', () => connect());
 
 const resetButton = document.getElementById('reset-button');
 resetButton.addEventListener('click', () => (window.location = '/'));
-
-// Realizar la solicitud a través de Azure API Management
-fetch('https://am-ecp-llm.azure-api.net/', {
-  method: 'GET',
-})
-  .then((response) => {
-    if (response.ok) {
-      // Procesa la respuesta aquí si es necesario
-      return response.json();
-    } else {
-      console.error('Error en la solicitud.');
-    }
-  })
-  .then((data) => {
-    // Maneja los datos de la respuesta JSON aquí si es necesario
-  })
-  .catch((error) => {
-    console.error('Error al hacer la solicitud:', error);
-  });
