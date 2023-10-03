5477f50e5b86f7102cc0ddd18b64ecbcbbf954c5
dalarcon80 <63214788+dalarcon80@users.noreply.github.com>
2023-10-02 19:16:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-// Enter your token server url to connect to your project
-// NOTE: your token server must allow requests from 'https://web-platform-9chn6x.stackblitz.io'
-//const tokenServer = 'http://localhost:5001/auth/authorize';
-
-// Note that your API key must allow requests from
-// this StackBlitz demo: 'https://web-platform-9chn6x.stackblitz.io'
-// Note that the URL will change if you fork this project
 const apiKey =
-  'eyJzb3VsSWQiOiJkZG5hLWRhbmllbC1hbGFyY29uLS1kaWdpdGFsLWNoaWVmLWV4cGVyaWVuY2UiLCJhdXRoU2VydmVyIjoiaHR0cHM6Ly9kaC5hei5zb3VsbWFjaGluZXMuY2xvdWQvYXBpL2p3dCIsImF1dGhUb2tlbiI6ImFwaWtleV92MV9lNmYxZTA3OS00ODI1LTQ2YjUtYjE5ZC05YjEwYzRjNDUzNjEifQ==';
+  'eyJzb3VsSWQiOiJkZG5hLWRhbmllbC1hbGFyY29uLS1kaWdpdGFsLWNoaWVmLWV4cGVyaWVuY2UiLCJhdXRoU2VydmVyIjoiaHR0cHM6Ly9kaC5hei5zb3VsbWFjaGluZXMuY2xvdWQvYXBpL2p3dCIsImF1dGhUb2tlbiI6ImFwaWtleV92MV9lNmY1ZTA3OS00ODI1LTQ2YjUtYjE5ZC05YjEwYzRjNDUzNjEifQ==';
 
 let scene;
 
-/**
- * Start a new connection.
- * Request a JWT from the token server and use it
- * to connect to the Soul Machines session server.
- */
 async function connect() {
-  // get the video element
   const videoEl = document.getElementById('sm-video');
-
-  // create a new scene object
   scene = new Scene({
     apiKey: apiKey,
     videoElement: videoEl,
@@ -27,31 +12,20 @@ async function connect() {
     requiredMediaDevices: { microphone: true, camera: true },
   });
 
-  // connect the Scene to the session server
   await scene
     .connect()
     .then((sessionId) => onConnectionSuccess(sessionId))
     .catch((error) => onConnectionError(error));
 }
 
-/**
- * Handle successful connection
- * On success, we must start the video.
- */
 function onConnectionSuccess(sessionId) {
   console.info('success! session id:', sessionId);
-
-  // start the video playing
   scene
     .startVideo()
     .then((videoState) => console.info('started video with state:', videoState))
     .catch((error) => console.warn('could not start video:', error));
 }
 
-/**
- * Handle failed connection
- * On error, we must display some feedback to the user
- */
 function onConnectionError(error) {
   switch (error.name) {
     case 'noUserMedia':
@@ -66,12 +40,24 @@ function onConnectionError(error) {
   }
 }
 
-/**
- * Event listeners for button in the HTML
- */
-
 const connectButton = document.getElementById('connect-button');
 connectButton.addEventListener('click', () => connect());
 
 const resetButton = document.getElementById('reset-button');
 resetButton.addEventListener('click', () => (window.location = '/'));
+
+// Realizar la solicitud en modo 'no-cors'
+fetch('https://dh.az.soulmachines.cloud/api/jwt', {
+  method: 'GET',
+  mode: 'no-cors',
+})
+  .then((response) => {
+    if (response.ok) {
+      console.log('Solicitud realizada con éxito.');
+    } else {
+      console.error('Error en la solicitud.');
+    }
+  })
+  .catch((error) => {
+    console.error('Error al hacer la solicitud:', error);
+  });
